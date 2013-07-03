f3cc173e53106ba93c07ac4d3c813793d84b5e33
W. Trevor King <wking@tremily.us>
2013-07-03 13:31:30 -0400

    Run update-copyright.py

--- pycalendar/dtype/datetime.py
+++ pycalendar/dtype/datetime.py
@@ -1,4 +1,18 @@
-# Copyright
+# Copyright (C) 2013 W. Trevor King <wking@tremily.us>
+#
+# This file is part of pycalender.
+#
+# pycalender is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# pycalender is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# pycalender.  If not, see <http://www.gnu.org/licenses/>.
 
 """Functions for processing dates with times
 
